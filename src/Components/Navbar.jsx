07599c8c96dece758025de7709af6cbf83cb1309
iu.medulashvili@gmail.com <iu.medulashvili@gmail.com>
2024-08-08 21:24:52 +0400

    first commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,37 +1,51 @@
-import { GpsFixed } from "@material-ui/icons"
+import { useState } from "react";
 
-const Navbar = ({setCategory}) => {
+const Navbar = ({ setCategory }) => {
+
+    const [isDark, setIsDark] = useState(false);
+    const toggleBackground = () => {
+        setIsDark(prevIsDark => !prevIsDark);
+        document.body.style.backgroundColor = isDark ? "#ffffff" : "#333333";
+    };
+
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark" style={{cursor:"pointer"}}>
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#"><span className="badge bg-light text-dark fs-5">News M</span></a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <div className="nav-link" onClick={() => setCategory("technology")}>Technology</div>
-        </li>
-        <li className="nav-item">
-          <div className="nav-link" onClick={() => setCategory("business")}>Business</div>
-        </li>
-        <li className="nav-item">
-          <div className="nav-link" onClick={() => setCategory("health")}>Health</div>
-        </li>
-        <li className="nav-item">
-          <div className="nav-link" onClick={() => setCategory("sports")}>Sports</div>
-        </li>
-        <li className="nav-item">
-          <div className="nav-link" onClick={() => setCategory("entertaiment")}>Entertaiment</div>
-        </li>
-        
-        
-      </ul>
-    </div>
-  </div>
-</nav>
-    )
+        <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark" style={{ cursor: "pointer" }}>
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#"><span className="badge bg-light text-dark fs-5">News M</span></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <div className="nav-link" onClick={() => setCategory("technology")}>Technology</div>
+                        </li>
+                        <li className="nav-item">
+                            <div className="nav-link" onClick={() => setCategory("business")}>Business</div>
+                        </li>
+                        <li className="nav-item">
+                            <div className="nav-link" onClick={() => setCategory("health")}>Health</div>
+                        </li>
+                        <li className="nav-item">
+                            <div className="nav-link" onClick={() => setCategory("sports")}>Sports</div>
+                        </li>
+                        <li className="nav-item">
+                            <div className="nav-link" onClick={() => setCategory("entertainment")}>Entertainment</div>
+                        </li>
+                    </ul>
+                        <div className="form-check form-switch ms-auto">
+                              <input 
+                              id="checkboxInput" 
+                              type="checkbox"
+                              onChange={toggleBackground}
+                              />
+                                <label className="toggleSwitch" for="checkboxInput">
+                                </label> 
+                          </div>
+                </div>
+            </div>
+        </nav>
+    );
 }
 
-export default Navbar
+export default Navbar;
